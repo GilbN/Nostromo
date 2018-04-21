cac0591eebbf4cfa0304158ea29b4837b0c51cb0
GilbN <gilberg@gmail.com>
2018-04-21 16:23:22 +0200

    Update ip_blacklist.py

--- Server/scripts/plexpy/ip_blacklist.py
+++ Server/scripts/plexpy/ip_blacklist.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
 '''
-Receive session_key and IP from PlexPy when playback starts. 
+Receive session_key and IP from Tautulli when playback starts. 
 Use IP to check against blacklist.
 If in blacklist use session_key to determine stream and kill.
 
-PlexPy > Settings > Notification Agents > Scripts > Bell icon:
+Tautulli > Settings > Notification Agents > Scripts > Bell icon:
         [X] Notify on playback start
-PlexPy > Settings > Notification Agents > Scripts > Gear icon:
+Tautulli > Settings > Notification Agents > Scripts > Gear icon:
         Playback Start: ip_blacklist.py
-PlexPy > Settings > Notifications > Script > Script Arguments:
+Tautulli > Settings > Notifications > Script > Script Arguments:
         {session_key} {ip_address}
 
 '''
@@ -17,24 +17,24 @@ PlexPy > Settings > Notifications > Script > Script Arguments:
 import sys
 import requests
 from plexapi.server import PlexServer
-# pip install plexapi
+import configparser
 
 
 ## EDIT THESE SETTINGS ##
-PLEX_TOKEN = 'xxxxxx'
+PLEX_TOKEN = 'xxxxxxxxx'
 PLEX_URL = 'http://localhost:32400'
 
-PLEXPY_APIKEY = 'xxxxxx'  # Your PlexPy API key
-PLEXPY_URL = 'http://localhost:8182/'  # Your PlexPy URL
+TAUTULLI_APIKEY = 'xxxxxxx'  # Your Tautulli API key
+TAUTULLI_URL = 'http://localhost:8181/'  # Your Tautulli URL
 
-IP_BLACKLIST = ['10.10.0.12']  # List IP addresses.
+IP_BLACKLIST = ['10.10.10.10']  # List IP addresses.
 IGNORE_LST = ('')  # List usernames that should be ignored.
 
 REASON = 'IP Address: {} was found in blacklist.'
 
-AGENT_ID = 14  # Notification agent ID for PlexPy
+NOTIFIER_ID = 14  # Notification agent ID for Tautulli
 # Find Notification agent ID here:
-# https://github.com/JonnyWong16/plexpy/blob/master/API.md#notify
+# Tautulli Settings -> NOTIFICATION AGENTS -> :bell: Agent (NotifierID - {Description)
 
 SUBJECT_TEXT = "IP Blacklist Violation"
 BODY_TEXT = "Killed {user}'s stream of {title}. IP: {ip} in blacklist"
@@ -56,23 +56,23 @@ def send_notification(subject_text, body_text):
     except LookupError as e:
         sys.stderr.write("Unable to substitute '{0}' in the notification subject or body".format(e))
         return None
-    # Send the notification through PlexPy
-    payload = {'apikey': PLEXPY_APIKEY,
+    # Send the notification through Tautulli
+    payload = {'apikey': TAUTULLI_APIKEY,
                'cmd': 'notify',
-               'agent_id': AGENT_ID,
+               'notifier_id': NOTIFIER_ID,
                'subject': subject,
                'body': body}
 
     try:
-        r = requests.post(PLEXPY_URL.rstrip('/') + '/api/v2', params=payload)
+        r = requests.post(TAUTULLI_URL.rstrip('/') + '/api/v2', params=payload)
         response = r.json()
 
         if response['response']['result'] == 'success':
-            sys.stdout.write("Successfully sent PlexPy notification.")
+            sys.stdout.write("Successfully sent Tautulli notification.")
         else:
             raise Exception(response['response']['message'])
     except Exception as e:
-        sys.stderr.write("PlexPy API 'notify' request failed: {0}.".format(e))
+        sys.stderr.write("Tautulli API 'notify' request failed: {0}.".format(e))
         return None
 
 
@@ -81,7 +81,11 @@ if ip_address in IP_BLACKLIST:
         username = session.usernames[0]
         title = (session.grandparentTitle + ' - ' if session.type == 'episode' else '') + session.title
         if session.sessionKey == int(sessionKey) and username not in IGNORE_LST:
-            sys.stdout.write("Killing {user}'s stream of {title}. IP: {ip} in blacklist".format(
+            sys.stdout.write("Killing {user}'s stream of {title}. IP: {ip} not in whitelist".format(
                 user=username, title=title, ip=ip_address))
             session.stop(reason=REASON.format(ip_address))
             send_notification(SUBJECT_TEXT, BODY_TEXT.format(user=username, ip=ip_address, title=title))
+        else:
+            sys.stdout.write('User: {} is ignored from this script.'.format(username))
+else:
+    sys.stdout.write('IP: {} is in whitelist, ignoring.'.format(ip_address))
